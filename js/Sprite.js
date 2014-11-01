1db433c7d5a54f59fe2ae06d7afa3110597a8804
Rúnar Berg Baugsson Sigríðarson <runarberg@zoho.com>
2014-11-01 16:18:57 +0000

    coord to box function

--- js/Sprite.js
+++ js/Sprite.js
@@ -7,9 +7,9 @@
 /* jshint browser: true, devel: true, globalstrict: true */
 
 /*
-0        1         2         3         4         5         6         7         8
-12345678901234567890123456789012345678901234567890123456789012345678901234567890
-*/
+ 0        1         2         3         4         5         6         7         8
+ 12345678901234567890123456789012345678901234567890123456789012345678901234567890
+ */
 
 
 // Construct a "sprite" from the given `image`,
@@ -32,7 +32,7 @@ Sprite.prototype.drawAt = function (ctx, x, y) {
 
 Sprite.prototype.drawCentredAt = function (ctx, cx, cy, rotation) {
     if (rotation === undefined) rotation = 0;
-    
+
     var w = this.width,
         h = this.height;
 
@@ -40,22 +40,22 @@ Sprite.prototype.drawCentredAt = function (ctx, cx, cy, rotation) {
     ctx.translate(cx, cy);
     ctx.rotate(rotation);
     ctx.scale(this.scale, this.scale);
-    
+
     // drawImage expects "top-left" coords, so we offset our destination
     // coords accordingly, to draw our sprite centred at the origin
     this.drawAt(ctx, -w/2, -h/2);
-    
+
     ctx.restore();
-};  
+};
 
 Sprite.prototype.drawWrappedCentredAt = function (ctx, cx, cy, rotation) {
-    
+
     // Get "screen width"
     var sw = g_canvas.width;
-    
+
     // Draw primary instance
     this.drawWrappedVerticalCentredAt(ctx, cx, cy, rotation);
-    
+
     // Left and Right wraps
     this.drawWrappedVerticalCentredAt(ctx, cx - sw, cy, rotation);
     this.drawWrappedVerticalCentredAt(ctx, cx + sw, cy, rotation);
@@ -65,10 +65,10 @@ Sprite.prototype.drawWrappedVerticalCentredAt = function (ctx, cx, cy, rotation)
 
     // Get "screen height"
     var sh = g_canvas.height;
-    
+
     // Draw primary instance
     this.drawCentredAt(ctx, cx, cy, rotation);
-    
+
     // Top and Bottom wraps
     this.drawCentredAt(ctx, cx, cy - sh, rotation);
     this.drawCentredAt(ctx, cx, cy + sh, rotation);
--- js/util.js
+++ js/util.js
@@ -9,125 +9,132 @@
 var util = {
 
 
-// RANGES
-// ======
+    // RANGES
+    // ======
 
-clampRange: function(value, lowBound, highBound) {
-    if (value < lowBound) {
-	value = lowBound;
-    } else if (value > highBound) {
-	value = highBound;
+    clampRange: function(value, lowBound, highBound) {
+        if (value < lowBound) {
+            value = lowBound;
+        } else if (value > highBound) {
+            value = highBound;
+        }
+        return value;
+    },
+
+    wrapRange: function(value, lowBound, highBound) {
+        while (value < lowBound) {
+            value += (highBound - lowBound);
+        }
+        while (value > highBound) {
+            value -= (highBound - lowBound);
+        }
+        return value;
+    },
+
+    isBetween: function(value, lowBound, highBound) {
+        if (value < lowBound) { return false; }
+        if (value > highBound) { return false; }
+        return true;
+    },
+
+
+    // RANDOMNESS
+    // ==========
+
+    randRange: function(min, max) {
+        return (min + Math.random() * (max - min));
+    },
+
+
+    // MISC
+    // ====
+
+    square: function(x) {
+        return x*x;
+    },
+
+
+    // DISTANCES
+    // =========
+
+    distSq: function(x1, y1, x2, y2) {
+        return this.square(x2-x1) + this.square(y2-y1);
+    },
+
+    wrappedDistSq: function(x1, y1, x2, y2, xWrap, yWrap) {
+        var dx = Math.abs(x2-x1),
+            dy = Math.abs(y2-y1);
+        if (dx > xWrap/2) {
+            dx = xWrap - dx;
+        };
+        if (dy > yWrap/2) {
+            dy = yWrap - dy;
+        }
+        return this.square(dx) + this.square(dy);
+    },
+
+
+    // CANVAS OPS
+    // ==========
+
+    clearCanvas: function (ctx) {
+        var prevfillStyle = ctx.fillStyle;
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.fillStyle = prevfillStyle;
+    },
+
+    strokeCircle: function (ctx, x, y, r) {
+        ctx.beginPath();
+        ctx.arc(x, y, r, 0, Math.PI * 2);
+        ctx.stroke();
+    },
+
+    fillCircle: function (ctx, x, y, r, style) {
+        if(style === undefined) {
+            style = ctx.fillStyle;
+        }
+        var oldStyle = ctx.fillStyle;
+        ctx.fillStyle = style;
+        ctx.beginPath();
+        ctx.arc(x, y, r, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.fillStyle = oldStyle;
+    },
+
+    fillBox: function (ctx, x, y, w, h, style) {
+        var oldStyle = ctx.fillStyle;
+        ctx.fillStyle = style;
+        ctx.fillRect(x, y, w, h);
+        ctx.fillStyle = oldStyle;
+    },
+
+    fillCenteredSquare: function (ctx, x, y, halfSquareDimension, style) {
+        this.fillBox(ctx, x - halfSquareDimension, y - halfSquareDimension, halfSquareDimension*2, halfSquareDimension*2, style);
+    },
+
+
+    //EXTRAS WITH NO HOME
+    drawYellowCircle: function (ctx, x, y, radius){
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+        ctx.fillStyle = 'yellow';
+        ctx.fill();
+    },
+
+    getCoordsFromBox: function(row, column) {
+        var dimension = consts.BOX_DIMENSION * consts.SCALING;
+        var xPos = column * dimension + dimension / 2;
+        var yPos = row * dimension + dimension / 2;
+        return {xPos: xPos, yPos: yPos};
+    },
+
+    getBoxFromCoord: function(x, y) {
+        var dimension = consts.BOX_DIMENSION * consts.SCALING;
+        var column = (x - dimension/2) / dimension;
+        var row = (y - dimension/2) / dimension;
+
+        return {row: row, column: column};
     }
-    return value;
-},
-
-wrapRange: function(value, lowBound, highBound) {
-    while (value < lowBound) {
-	value += (highBound - lowBound);
-    }
-    while (value > highBound) {
-	value -= (highBound - lowBound);
-    }
-    return value;
-},
-
-isBetween: function(value, lowBound, highBound) {
-    if (value < lowBound) { return false; }
-    if (value > highBound) { return false; }
-    return true;
-},
-
-
-// RANDOMNESS
-// ==========
-
-randRange: function(min, max) {
-    return (min + Math.random() * (max - min));
-},
-
-
-// MISC
-// ====
-
-square: function(x) {
-    return x*x;
-},
-
-
-// DISTANCES
-// =========
-
-distSq: function(x1, y1, x2, y2) {
-    return this.square(x2-x1) + this.square(y2-y1);
-},
-
-wrappedDistSq: function(x1, y1, x2, y2, xWrap, yWrap) {
-    var dx = Math.abs(x2-x1),
-	dy = Math.abs(y2-y1);
-    if (dx > xWrap/2) {
-	dx = xWrap - dx;
-    };
-    if (dy > yWrap/2) {
-	dy = yWrap - dy;
-    }
-    return this.square(dx) + this.square(dy);
-},
-
-
-// CANVAS OPS
-// ==========
-
-clearCanvas: function (ctx) {
-    var prevfillStyle = ctx.fillStyle;
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.fillStyle = prevfillStyle;
-},
-
-strokeCircle: function (ctx, x, y, r) {
-    ctx.beginPath();
-    ctx.arc(x, y, r, 0, Math.PI * 2);
-    ctx.stroke();
-},
-
-fillCircle: function (ctx, x, y, r, style) {
-    if(style === undefined) {
-        style = ctx.fillStyle;
-    }
-    var oldStyle = ctx.fillStyle;
-    ctx.fillStyle = style;
-    ctx.beginPath();
-    ctx.arc(x, y, r, 0, Math.PI * 2);
-    ctx.fill();
-    ctx.fillStyle = oldStyle;
-},
-
-fillBox: function (ctx, x, y, w, h, style) {
-    var oldStyle = ctx.fillStyle;
-    ctx.fillStyle = style;
-    ctx.fillRect(x, y, w, h);
-    ctx.fillStyle = oldStyle;
-},
-
-fillCenteredSquare: function (ctx, x, y, halfSquareDimension, style) {
-    this.fillBox(ctx, x - halfSquareDimension, y - halfSquareDimension, halfSquareDimension*2, halfSquareDimension*2, style);
-},
-
-
-//EXTRAS WITH NO HOME
-drawYellowCircle: function (ctx, x, y, radius){
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
-    ctx.fillStyle = 'yellow';
-    ctx.fill();
-},
-
-getCoordsFromBox: function(row, column) {
-    var dimension = consts.BOX_DIMENSION * consts.SCALING;
-    var xPos = column * dimension + dimension / 2;
-    var yPos = row * dimension + dimension / 2;
-    return {xPos: xPos, yPos: yPos};
-}
-
 
 };
