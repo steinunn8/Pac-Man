b948c4b42ae5ac51571ff449e9fc2888f094cf86
Rúnar Berg Baugsson Sigríðarson <runarberg@zoho.com>
2014-11-01 14:24:33 +0000

    No error on start-up

--- js/entityManager.js
+++ js/entityManager.js
@@ -1,23 +1,23 @@
 /*
 
-entityManager.js
+ entityManager.js
 
-A module which handles arbitrary entity-management for "Asteroids"
+ A module which handles arbitrary entity-management for "Asteroids"
 
 
-We create this module as a single global object, and initialise it
-with suitable 'data' and 'methods'.
+ We create this module as a single global object, and initialise it
+ with suitable 'data' and 'methods'.
 
-"Private" properties are denoted by an underscore prefix convention.
+ "Private" properties are denoted by an underscore prefix convention.
 
-*/
+ */
 
 
 "use strict";
 
 
 // Tell jslint not to complain about my use of underscore prefixes (nomen),
-// my flattening of some indentation (white), or my use of incr/decr ops 
+// my flattening of some indentation (white), or my use of incr/decr ops
 // (plusplus).
 //
 /*jslint nomen: true, white: true, plusplus: true*/
@@ -25,92 +25,93 @@ with suitable 'data' and 'methods'.
 
 var entityManager = {
 
-// "PRIVATE" DATA
+    // "PRIVATE" DATA
 
-_ghosts   : [],
+    _ghosts   : [],
+    _maze     : [],
+    
 
-// "PRIVATE" METHODS
+    // "PRIVATE" METHODS
 
-_generateGhosts: function() {
-    // TODO
-},
+    _generateGhosts: function() {
+        // TODO
+    },
 
-_forEachOf: function(aCategory, fn) {
-    for (var i = 0; i < aCategory.length; ++i) {
-        fn.call(aCategory[i]);
-    }
-},
-
-// PUBLIC METHODS
-
-// A special return value, used by other objects,
-// to request the blessed release of death!
-//
-KILL_ME_NOW : -1,
-
-// Some things must be deferred until after initial construction
-// i.e. thing which need `this` to be defined.
-//
-deferredSetup : function () {
-    this._categories = [this._rocks, this._bullets, this._ships];
-},
-
-init: function() {
-    this._generateGhosts();
-    //this._generatePacman();
-},
-
-generateGhost : function(descr) {
-//    this._ghosts.push(new Ghost(descr));
-},
-
-generatePacman : function(descr) {
-//    this._ships.push(new Ship(descr));
-},
-
-update: function(du) {
-
-    for (var c = 0; c < this._categories.length; ++c) {
-
-        var aCategory = this._categories[c];
-        var i = 0;
-
-        while (i < aCategory.length) {
-
-            var status = aCategory[i].update(du);
-
-            if (status === this.KILL_ME_NOW) {
-                // remove the dead guy, and shuffle the others down to
-                // prevent a confusing gap from appearing in the array
-                aCategory.splice(i,1);
-            }
-            else {
-                ++i;
-            }
-        }
-    }
-},
-
-render: function(ctx) {
-
-    var debugX = 10, debugY = 100;
-
-    for (var c = 0; c < this._categories.length; ++c) {
-
-        var aCategory = this._categories[c];
-
+    _forEachOf: function(aCategory, fn) {
         for (var i = 0; i < aCategory.length; ++i) {
-
-            aCategory[i].render(ctx);
-            //debug.text(".", debugX + i * 10, debugY);
-
+            fn.call(aCategory[i]);
         }
-        debugY += 10;
+    },
+
+    // PUBLIC METHODS
+
+    // A special return value, used by other objects,
+    // to request the blessed release of death!
+    //
+    KILL_ME_NOW : -1,
+
+    // Some things must be deferred until after initial construction
+    // i.e. thing which need `this` to be defined.
+    //
+    deferredSetup : function () {
+        this._categories = [this._ghosts, this._maze];
+    },
+
+    init: function() {
+        this._generateGhosts();
+        //this._generatePacman();
+    },
+
+    generateGhost : function(descr) {
+        //    this._ghosts.push(new Ghost(descr));
+    },
+
+    generatePacman : function(descr) {
+        //    this._ships.push(new Ship(descr));
+    },
+
+    update: function(du) {
+
+        for (var c = 0; c < this._categories.length; ++c) {
+
+            var aCategory = this._categories[c];
+            var i = 0;
+
+            while (i < aCategory.length) {
+
+                var status = aCategory[i].update(du);
+
+                if (status === this.KILL_ME_NOW) {
+                    // remove the dead guy, and shuffle the others down to
+                    // prevent a confusing gap from appearing in the array
+                    aCategory.splice(i,1);
+                }
+                else {
+                    ++i;
+                }
+            }
+        }
+    },
+
+    render: function(ctx) {
+
+        var debugX = 10, debugY = 100;
+
+        for (var c = 0; c < this._categories.length; ++c) {
+
+            var aCategory = this._categories[c];
+
+            for (var i = 0; i < aCategory.length; ++i) {
+
+                aCategory[i].render(ctx);
+                //debug.text(".", debugX + i * 10, debugY);
+
+            }
+            debugY += 10;
+        }
     }
-}
 
 }
 
 // Some deferred setup which needs the object to have been created first
 entityManager.deferredSetup();
-
